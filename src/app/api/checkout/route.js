f090aaf5c109fde1669be720184097c01c9338d1
Mos amon <amonchanchai@gmail.com>
2024-02-29 18:29:37 +0700

    feat:paypalChangeCost

---
+++ src/app/api/checkout/route.js
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import paypal from "@paypal/checkout-server-sdk";
+
+const clientId =
+  "ARxGPZRXhehw8ZPR_DcLN6Hjn02IoVfo68gXE5AdORvBrmSZIoPIebuoRlarYoRUdPWKLdhU74lPHmTa";
+const clientSecret =
+  "ENUSkypRwqarVfI3FFMpuvLxSFmIjjuIOtzLmPBiSFySrKK9MyMXP_ZuDSz8xibgv3wFEjKAbBzq42OK";
+
+const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+const client = new paypal.core.PayPalHttpClient(environment);
+
+export async function POST() {
+  const request = new paypal.orders.OrdersCreateRequest();
+  request.requestBody({
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "THB",
+          value: "100.00",
+          breakdown: {
+            item_total: {
+              currency_code: "THB",
+              value: "100.00",
+            },
+          },
+        },
+        items: [
+          {
+            name: "Aa",
+            description: "Aaaaa",
+            quantity: "1",
+            unit_amount: {
+              currency_code: "THB",
+              value: "50.00",
+            },
+          },
+          {
+            name: "Bb",
+            description: "Bbbb",
+            quantity: "1",
+            unit_amount: {
+              currency_code: "THB",
+              value: "50.00",
+            },
+          },
+        ],
+      },
+    ],
+  });
+  const response = await client.execute(request);
+  console.log(response);
+  return NextResponse.json({
+    id: response.result.id,
+  });
+}
--- src/components/CheckoutPaypal.jsx
+++ src/components/CheckoutPaypal.jsx
@@ -1,12 +1,28 @@
-"useClient"
+"useClient";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-const  CheckoutPaypal = () => {
-    return (
-        <PayPalScriptProvider options={{ clientId: "test" }}>
-            <PayPalButtons style={{ layout: "horizontal" }} />
-        </PayPalScriptProvider>
-    );
-}
+const CheckoutPaypal = () => {
+  return (
+    <PayPalScriptProvider
+      options={{
+        clientId:
+          "ARxGPZRXhehw8ZPR_DcLN6Hjn02IoVfo68gXE5AdORvBrmSZIoPIebuoRlarYoRUdPWKLdhU74lPHmTa",
+      }}
+    >
+      <PayPalButtons
+        style={{ layout: "horizontal", color: "blue" }}
+        createOrder={async () => {
+          const res = await fetch("/api/checkout", { method: "POST" });
+          const order = await res.json();
+          console.log(order);
+          return order.id;
+        }}
+      />
+    </PayPalScriptProvider>
+  );
+};
 
-export default CheckoutPaypal
+export default CheckoutPaypal;
+//         createOrder={() => {}}
+//         onCancel={() => {}}
+//         onApprove={() => {}}
