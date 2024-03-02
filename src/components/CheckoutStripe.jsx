d51090692330552fceaf99e96bc792bb1bd1a010
Mos amon <amonchanchai@gmail.com>
2024-03-02 23:45:44 +0700

    rebase

--- src/components/CheckoutStripe.jsx
+++ src/components/CheckoutStripe.jsx
@@ -2,16 +2,97 @@ import React from "react";
 import {
   PaymentElement,
   useStripe,
-  useElements
+  useElements,
 } from "@stripe/react-stripe-js";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useSelector } from "react-redux";
+import { supabase } from "../../supabase";
+import { getCookie } from "cookies-next";
+
+import Container from "./Container";
 
 export default function CheckoutStripe() {
+  const totalPrice = useSelector((state) => state.total);
   const stripe = useStripe();
   const elements = useElements();
-
+  const search = useSearchParams();
+  const router = useRouter();
+  const services = useSelector((state) => state.services);
+  const step = search.get("step") ? parseInt(search.get("step")) : 0;
+  const total = useSelector((state) => state.total);
+  const address = useSelector((state) => state.address);
+  const remainingQuota = useSelector((state) => state.remainingQuota);
+  const promotionCode = useSelector((state) => state.promotionCode);
+  const userData = getCookie("user");
+  const userId = userData ? JSON.parse(userData) : {};
 
   const [message, setMessage] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
+
+  function generateOrderCode() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let orderCode = "";
+    for (let i = 0; i < 8; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      orderCode += characters.charAt(randomIndex);
+    }
+    return orderCode;
+  }
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const { error } = await supabase
+      .from("promotions")
+      .update([
+        {
+          remaining_quota: Number(remainingQuota) - 1,
+        },
+      ])
+      .eq("promotion_code", promotionCode);
+
+    if (error) {
+      console.error(
+        "Error inserting data into 'promotion' table:",
+        error.message
+      );
+      return;
+    }
+
+    const userData = getCookie("user");
+    const userId = userData ? JSON.parse(userData) : {};
+
+    const { data: ordersData, error: ordersError } = await supabase
+      .from("orders")
+      .insert([
+        {
+          user_id: userId.id,
+          select_services: services,
+          total_price: parseInt(total),
+          service_date: address.date,
+          status: "รอดำเนินการ",
+          service_time: `${address.hour}:${address.minute}`,
+          province: address.province,
+          district: address.amphoe,
+          sub_district: address.district,
+          address_no: address.address,
+          remark: address.more,
+          order_code: generateOrderCode(),
+        },
+      ])
+      .select();
+
+    console.log(ordersData);
+
+    router.push("/paymentsuccess");
+
+    if (ordersError) {
+      console.error(
+        "Error inserting data into 'orders' table:",
+        ordersError.message
+      );
+      return;
+    }
+  };
 
   React.useEffect(() => {
     if (!stripe) {
@@ -57,10 +138,8 @@ export default function CheckoutStripe() {
 
     const { error } = await stripe.confirmPayment({
       elements,
-      redirect:"if_required",
-      confirmParams: {
-        
-      },
+      redirect: "if_required",
+      confirmParams: {},
     });
 
     // This point will only be reached if there is an immediate error when
@@ -75,6 +154,7 @@ export default function CheckoutStripe() {
     // }
 
     setIsLoading(false);
+    handleUpdate(e);
   };
 
   const paymentElementOptions = {
@@ -83,12 +163,16 @@ export default function CheckoutStripe() {
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
-
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
+      <button
+        disabled={isLoading || !stripe || !elements}
+        id="submit"
+        className="text-white bg-[#2563eb] pb-2.5 pt-2.5 pr-10 pl-10 text-center absolute rounded-xl bottom-1 right-56 z-50"
+      >
         <span id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
+        </span>{" "}
+        <span>{totalPrice} ฿</span>
       </button>
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
