bb01ece9f3cdd569061a34f9ec05e5155666ea71
Parunyu Pitragool <Mek@Meks-MacBook-Pro.local>
2024-03-05 06:01:01 +0700

    fix some bug for deploy

--- src/app/user/orders/page.jsx
+++ src/app/user/orders/page.jsx
@@ -2,7 +2,7 @@
 import Navbar from "@/components/Navbar";
 import Copyright from "@/components/Copyright";
 import Footer from "@/components/Footer";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { createClient } from "@/supabase/client";
 import { getCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
