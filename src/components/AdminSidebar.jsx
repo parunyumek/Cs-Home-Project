bb45c9da8b1c19785fddd9ed68da5c9c4e73ae3f
bodin <bodinsonsawan@gmail.com>
2024-03-04 12:03:50 +0700

    edit(AdminSidebar.jsx):add back to homepage button

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -5,14 +5,16 @@ const AdminSideBar = () => {
   return (
     <div className="w-60 h-screen bg-blue-950 flex flex-col fixed z-20">
       <div className="w-48 h-[46px] bg-indigo-50 rounded-xl mt-6 ml-6">
-        <div className="text-blue-600 text-xl font-medium flex justify-center mt-2">
-          <img
-            src="/assets/icons/house_1.svg"
-            alt="house-icon"
-            className="mr-2 mb-4 w-[26.6px] h-[26.6px]"
-          />
-          HomeServices
-        </div>
+        <Link href={"http://localhost:3000/"}>
+          <div className="text-blue-600 text-xl font-medium flex justify-center mt-2">
+            <img
+              src="/assets/icons/house_1.svg"
+              alt="house-icon"
+              className="mr-2 mb-4 w-[26.6px] h-[26.6px]"
+            />
+            HomeServices
+          </div>
+        </Link>
       </div>
       <div className="flex flex-col text-zinc-100 mt-11 text-base font-medium leading-normal ">
         <Link
